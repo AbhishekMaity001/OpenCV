bf51cd595b404c78a221d6c537eb91102dbb0742
Abhishek <abhishek.maity97@gmail.com>
2021-01-13 23:22:57 +0530

    added main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ cap = cv2.VideoCapture(0)
 while True:
 
     _, frame = cap.read()
-    frame = cv2.flip(frame,1)
+    frame = cv2.flip(frame,1) # flipping the frame
     cv2.imshow("window",frame)
 
     if cv2.waitKey(1) == 27:
